065cf9f380bd95d9a0cf8f32724bec33108b230b
yshypat <yuliia.shypat@algoteque.com>
2019-06-14 23:41:53 +0200

    Custom visualization init

---
+++ src/_intefaces_/ship.inteface.ts
@@ -0,0 +1,10 @@
+export interface Ship {
+    x: number;
+    y: number;
+    xSpeed: number;
+    ySpeed: number;
+    width: number;
+    height: number;
+    angle: number;
+    zIndex: number;
+}
